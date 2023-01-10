3e4cce05c828cb4311dff2ca984f35d94a5cf913
muhammad arham <arhamymr@gmail.com>
2023-01-10 20:11:18 +0800

    chore(router): rename variable

--- router/route.go
+++ router/route.go
@@ -7,21 +7,21 @@ import (
 )
 
 func InitRoute() echo.Echo {
-	e := *echo.New()
+	r := *echo.New()
 
-	e.Static("/static", "static")
+	r.Static("/static", "static")
 
 	// auth
-	auth := e.Group("/auth")
+	auth := r.Group("/auth")
 	auth.POST("/register", handlers.Register)
 	auth.POST("/login", handlers.Login)
 
 	// post
-	post := e.Group("/posts")
+	post := r.Group("/posts")
 	post.GET("", handlers.GetMany)
 	post.GET("/:id", handlers.GetOne)
 	post.POST("/:id", handlers.Update)
 	post.DELETE("/:id", handlers.Delete)
 
-	return e
+	return r
 }
